45b277e2bf88f78dfc28ee8057281d2decc8957f
이윤호 <96047001+Leeyoonho02@users.noreply.github.com>
2024-01-07 12:51:39 +0900

    [EDIT]

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron')              /* Get modules       */
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -12,3 +12,7 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow()
 })
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
