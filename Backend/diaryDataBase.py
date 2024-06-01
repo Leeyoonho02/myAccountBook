8298a858dac78accecdfa169272088027f06e47c
Leeyoonho <hoya071878@gmail.com>
2024-06-01 18:56:40 +0900

    [NEW] Create DB class

---
+++ Backend/diaryDataBase.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+class diaryDataBase:
+    def __init__(self, dbName):
+        self.conn = sqlite3.connect(dbName)
+        self.c = self.conn.cursor()
+
+    def createTable(self):
+        self.c.execute('''
+                CREATE TABLE IF NOT EXISTS pages (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date TEXT,
+                    thankful TEXT,
+                    regret TEXT,
+                    study TEXT,
+                    exercise TEXT
+                )
+                ''')
+        self.conn.commit()
+
+    def insert(self, date, thankful, regret, study, exercise):
+        self.c.execute('''
+            INSERT INTO entries (date, thankful, regret, study, exercise)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (date, thankful, regret, study, exercise))
+        self.conn.commit()
+    
+    def update(self, date, field, value):
+        self.c.execute(f'UPDATE entries SET {field} = ? WHERE id = ?', (value, date))
+        self.conn.commit()
+        
+    def getEntry(self):
+        self.c.execute('SELECT * FROM entries')
+        return self.c.fetchall()
+    
+    def getEntryByDate(self, date):
+        self.c.execute('SELECT * FROM entries WHERE date =?', (date))
+        return self.c.fetchone()
+    
+    def closeTable(self):
+        self.conn.close()
